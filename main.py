06f54634565600e7ae8dfc501991c5bafbb08f28
MasamioNakada <mnakadab@unac.edu.pe>
2022-06-23 17:02:51 -0500

    normalization

--- main.py
+++ main.py
@@ -11,14 +11,21 @@ data = Data(path="in")
 
 
 if __name__ == "__main__":
+    #cargamos del directorio in los dataframes
     try:
         labels = data.get_labels(path="in")
         data_dict = load.load_datasets_from_csv(path="in")
     except ValueError:
         say.cow_says_error('Error al cargar base de datos')
     
+    #convertimos las columnas fechas a objeto -> datetime
     resume.dataset_date_iter(data_dict, labels)
+    
     data_dict = id_to_int_iter(data_dict, labels)
+
+    #normalizar los dataframes
+
+    #imprimos el resumen de los dataframes
     resume.resume_dataframe(data_dict, labels)
 
 
